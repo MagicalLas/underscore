20fa504e62336e5cd599619038deafe1f30dbcab
Las <haho013039@gmail.com>
2021-04-25 14:00:18 +0900

    Implement underscore lambda for add

--- tests/test_underscore.py
+++ tests/test_underscore.py
@@ -6,3 +6,9 @@ def test_underscore_id_function():
 
     assert f(1) == 1
     assert f("123") == "123"
+
+
+def test_underscore_add_number():
+    f = _ + 10
+
+    assert f(1) == 11
--- underscore/underscore.py
+++ underscore/underscore.py
@@ -2,5 +2,10 @@ class Underscore:
     def __call__(self, value):
         return value
 
+    def __add__(self, number):
+        def wrapper(value):
+            return value + number
+        return wrapper
+
 
 _ = Underscore()
